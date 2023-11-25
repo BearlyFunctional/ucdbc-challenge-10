4d57a180ea07fa497af93bfd55381845a1eeb6a9
BearlyFunctional <hjbell123.hb@gmail.com>
2023-11-25 12:22:56 -0800

    added code to generate svg file based on user input.

---
+++ index.js
@@ -0,0 +1,48 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+inquirer 
+    .prompt([
+        {
+            name: 'text',
+            message: 'Please enter three letters for your logo: '
+        },
+        {
+            name: 'textColor',
+            message: "Please enter the text's color (or it's hexidecimel number): "
+        },
+        {
+            type: 'list',
+            choices: ['circle', 'square', 'triangle'],
+            name: 'shape',
+            message: 'Please select an icon shape: '
+        },
+        {
+            name: 'shapeColor',
+            message: "Please enter the icon's color (or it's hexidecimel number): "
+        }
+    ]) .then((results) => {
+        generateSvg(results)
+    })
+
+const generateSvg = (userInput) => {
+    const {text, textColor, shape, shapeColor} = userInput
+    let shapeCode
+
+    if (shape == 'square') {
+        shapeCode = `<rect x="75" y="10" width="150" height="150" stroke='black' stroke-width='3' fill="${shapeColor}" />`
+    } else if (shape == 'circle') {
+        shapeCode = `<circle cx="150" cy="90" r="80" stroke='black' stroke-width='3' fill="${shapeColor}" />`
+    } else if (shape == 'triangle') {
+        shapeCode = `<polygon points="150, 10 290, 115 10, 115" stroke='black' stroke-width='3' fill="${shapeColor}" />`
+    }
+
+    const svgTemplate = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n\t${shapeCode}\n\t<text x="150" y="100" font-size="60" text-anchor="middle" stroke='black' stroke-width='.5' fill="${textColor}">${text}</text>\n</svg>`
+    fs.writeFile('./output/generatedIcon.svg', svgTemplate, err => {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('Icon created! Check output folder.')
+        }
+    })
+}
